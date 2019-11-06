5a2e154d9b83d392839bdb3c7db3aab3a7797975
Maximilian Mayer <maximilian_mayer@gmx.net>
2019-11-06 19:50:54 +0100

    adds and fixes test for validation of title for papers

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before(:each) do
+    @paper = FactoryBot.create :paper
+  end
+
+  it "should be invalid to create a paper with no title" do
+    expect(@paper).to be_valid
+
+    invalid_paper = Paper.new({"title" => nil, "venue" => "", "year" => 0})
+
+    expect(invalid_paper).to_not be_valid
+  end
 end
