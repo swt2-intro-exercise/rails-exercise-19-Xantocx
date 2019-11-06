9131dda2148fa88da49807ee6fbcbe53bf25c71c
Maximilian Mayer <maximilian_mayer@gmx.net>
2019-11-06 21:14:30 +0100

    adds support for the year parameter in the URL

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -2,6 +2,8 @@ class Paper < ApplicationRecord
 
     has_and_belongs_to_many :authors
 
+    scope :created_in, ->(year) { where("year == ?", year) }
+
     validates :title, presence: true, length: {minimum: 1}
     validates :venue, presence: true, length: {minimum: 1}
     validates :year, presence: true, numericality: { only_integer: true }
