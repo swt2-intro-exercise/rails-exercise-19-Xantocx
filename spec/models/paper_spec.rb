f240362046a609a7d6eaf87ab459a694bb61e424
Maximilian Mayer <maximilian_mayer@gmx.net>
2019-11-06 20:01:55 +0100

    adds and fixes test for extended year validation for papers

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -12,9 +12,11 @@ RSpec.describe Paper, type: :model do
     invalid_paper1 = Paper.new({"title" => nil, "venue" => "test", "year" => 0})
     invalid_paper2 = Paper.new({"title" => "test", "venue" => nil, "year" => 0})
     invalid_paper3 = Paper.new({"title" => "test", "venue" => "test", "year" => nil})
+    invalid_paper4 = Paper.new({"title" => "test", "venue" => "test", "year" => "test"})
 
     expect(invalid_paper1).to_not be_valid
     expect(invalid_paper2).to_not be_valid
     expect(invalid_paper3).to_not be_valid
+    expect(invalid_paper4).to_not be_valid
   end
 end
